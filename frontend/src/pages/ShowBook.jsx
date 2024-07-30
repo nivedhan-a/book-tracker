d639d8f624b985d1ccf205f54caf012afd821bca
nivedhan-a <nivedhan9895@gmail.com>
2024-07-30 21:31:51 +0530

    update mongourl

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/books/${id}`)
+      .get(`https://book-tracker-backend-six.vercel.app/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
