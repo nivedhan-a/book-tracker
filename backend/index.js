9ddd425a55c2be2a86fabdcd7eb5b706dac58849
nivedhan-a <nivedhan9895@gmail.com>
2024-07-31 01:17:52 +0530

    update url

--- backend/index.js
+++ backend/index.js
@@ -11,22 +11,20 @@ const app = express();
 app.use(express.json());
 
 // Middleware for handling CORS POLICY
-// Option 1: Allow All Origins with Default of cors(*)
-//app.use(cors());
-// Option 2: Allow Custom Origins
 app.use(
   cors({
-    origin: 'https://book-tracker-frontend-chi.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-   allowedHeaders: ['Content-Type'],
+    origin: 'https://book-tracker-frontend-chi.vercel.app', // Allow your frontend origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    allowedHeaders: ['Content-Type'], // Specify allowed headers
+    credentials: true, // Allow credentials to be sent
   })
 );
 
 app.get('/', (request, response) => {
   console.log(request);
-  return response.status(234).send('hey hey');
+  return response.status(200).send('hey hey'); // Use 200 for success
 });
-  
+
 app.use('/books', booksRoute);
 
 mongoose
